4771f17bee9212e8a522517412042073737f9231
Thilina Ilesinghe <75578256+thilina98@users.noreply.github.com>
2022-06-10 18:30:49 +0530

    Test (#6)
    * OpeningWindow and the VerificationPage if finished. issues with the files has been solved.
    
    * check button in the VerificationPage open the view of optionsfragment now

--- app/src/main/java/com/example/microbankingsystem/ui/VerificationPage.java
+++ app/src/main/java/com/example/microbankingsystem/ui/VerificationPage.java
@@ -2,15 +2,35 @@ package com.example.microbankingsystem.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.microbankingsystem.R;
 
 public class VerificationPage extends AppCompatActivity {
 
+    Button btn_verification_check;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification_page);
+
+        btn_verification_check = findViewById(R.id.btn_verification_check);
+
+        btn_verification_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openOptionsFragment();
+            }
+        });
     }
+
+    private void openOptionsFragment(){
+        Intent intent = new Intent(this, OptionsFragment.class);
+        startActivity(intent);
+    }
+
 }
