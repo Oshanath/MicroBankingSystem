11159ba77b6431651653f88bdba3336ae2e516bb
thilina98 <thilina.avishka@gmail.com>
2022-06-10 14:15:35 +0530

    just started

---
+++ app/src/main/java/com/example/microbankingsystem/ui/OpeningWindow.java
@@ -0,0 +1,14 @@
+package com.example.microbankingsystem.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpeningWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_window);
+    }
+}
