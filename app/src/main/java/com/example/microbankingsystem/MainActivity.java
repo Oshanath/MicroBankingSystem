d0a01e182ee49124a7b1343b9d9adb64d8703298
thilina98 <thilina.avishka@gmail.com>
2022-06-10 16:25:38 +0530

    OpeningWindow and the VerificationPage if finished. to be tested.

--- app/src/main/java/com/example/microbankingsystem/MainActivity.java
+++ app/src/main/java/com/example/microbankingsystem/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.microbankingsystem;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.microbankingsystem.ui.OpeningWindow;
 import com.example.microbankingsystem.ui.OptionsFragment;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -27,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
     Button btn_test;
+    Button btn_transaction_normal;
+    Button btn_transaction_critical;
 
       @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +52,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // initializing of buttons
         btn_test = findViewById(R.id.button_test);
+        btn_transaction_normal = findViewById(R.id.btn_transaction_normal);
+        btn_transaction_critical = findViewById(R.id.btn_transaction_critical);
 
+        // calling setOnClickListner on initialized buttons
         btn_test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 testact();
             }
         });
+
+        btn_transaction_normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openOpeningWindow();
+            }
+        });
+
+          btn_transaction_critical.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  openOpeningWindow();
+              }
+          });
+
     }
 
     @Override
@@ -91,5 +113,10 @@ public class MainActivity extends AppCompatActivity {
     public void testact(){
         Intent test = new Intent(this, OptionsFragment.class);
         startActivity(test);
+    }
+
+    private void openOpeningWindow(){
+        Intent intent = new Intent(this, OpeningWindow.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/microbankingsystem/ui/VerificationPage.java
+++ app/src/main/java/com/example/microbankingsystem/ui/VerificationPage.java
@@ -2,19 +2,15 @@ package com.example.microbankingsystem.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.example.microbankingsystem.R;
 
-
-public class OpeningWindow extends AppCompatActivity {
+public class VerificationPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opening_window);
+        setContentView(R.layout.activity_verification_page);
     }
 }
